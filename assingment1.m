bc3581d4baea3c1217d595a4b63a408f1db442c5
Rtmurphy981 <rtmurphy@wpi.edu>
2026-07-09 15:30:00 -0400

    Add assingment1.m

---
+++ assingment1.m
@@ -0,0 +1,5 @@
+a = input('Enter first number: ');
+b = input('Enter second number: ');
+
+disp(['the sum of the two inputs is ' num2str(a+b)])
+disp(['the product of the two inputs is ' num2str(a*b)])
